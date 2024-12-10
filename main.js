bbc475897e0dfc5bb7d6907f116a01371ee12804
Steven Brown <Stevenjbrown95@gmail.com>
2024-12-10 16:26:54 -0700

    3

---
+++ main.js
@@ -0,0 +1,14 @@
+const books = [
+    "Matthew", "Mark", "Luke", "John", "Acts", "Romans", "1 Corinthians", "2 Corinthians",
+    "Galatians", "Ephesians", "Philippians", "Colossians", "1 Thessalonians", "2 Thessalonians",
+    "1 Timothy", "2 Timothy", "Titus", "Philemon", "Hebrews", "James", "1 Peter", "2 Peter",
+    "1 John", "2 John", "3 John", "Jude", "Revelation"
+];
+
+const dropdown = document.getElementById('chapterDropdown');
+chapters.forEach(chapter => {
+    const option = document.createElement('option');
+    option.value = chapter;
+    option.textContent = chapter;
+    dropdown.appendChild(option);
+});
